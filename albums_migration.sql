fad81239c2f121881e9d25e97560f5d21631eb6e
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-29 16:53:18 -0400

    feat: tables

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+    CREATE TABLE albums (
+        id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+        artist VARCHAR(50) NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        release_date INT,
+        sales float,
+        genre VARCHAR(50),
+        PRIMARY KEY(id)
+    );
