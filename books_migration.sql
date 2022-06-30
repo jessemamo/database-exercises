c03621e9b380194902d652529a770004db0cc111
Jesse Mammosser <jesse.mamo123@gmail.com>
2022-06-30 10:58:28 -0400

    feat: mysql warm-up

---
+++ books_migration.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+
+CREATE TABLE books
+(
+
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    date_published DATE NOT NULL,
+    description TEXT,
+    bestseller_weeks INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+
+
+);
